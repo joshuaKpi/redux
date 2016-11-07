057d03716aaedd2e6767151e4706a9f02f03263e
V.Zinkevichius <joshua.kpi@gmail.com>
2016-11-07 18:55:57 +0200

    Preparations completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
@@ -16,5 +17,26 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ]
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+            path.resolve(__dirname, "src")
+        ]
+      }
+    ],
+    loaders: [
+      {
+        loaders: ['babel-loader'],
+        include: [
+            path.resolve(__dirname, 'src')
+        ],
+        test: /\.jsx?$/,
+        plugins: ['transform-runtime']
+      }
+    ]
+  }
 }
